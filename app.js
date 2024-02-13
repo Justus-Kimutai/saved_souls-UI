65fd7e3f65487a75e01d2997d201d2ca55e83962
Justus-Kimutai <123356955+Justus-Kimutai@users.noreply.github.com>
2024-02-13 09:53:27 +0300

    setup basic

---
+++ app.js
@@ -0,0 +1,26 @@
+const submitBtn = document.querySelector('.submit-btn')
+
+
+const backEndURL = 'https://evangelism.onrender.com'
+
+submitBtn.addEventListener('click',async ()=>{
+    const uname = document.getElementById('name').value
+    const phone = document.getElementById('phone').value
+    const region = document.getElementById('region').value
+    const village = document.getElementById('village').value
+
+    try {
+        await fetch(`https://evangelism.onrender.com/save-soul`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ uname, phone, region, village }),
+        });
+        alert('Post added successfully!');
+      } catch (error) {
+        console.error(error);
+      }
+})
+
+
